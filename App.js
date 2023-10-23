c6fcc0c7367ab0b85f2d0a9f1ddfbba3e88577ea
Misha <109940307+Alvinosa@users.noreply.github.com>
2023-10-23 10:10:57 +0700

    awalan

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+    return (
+        <View style={{backgroundColor: 'pink', flex: 1}}>
+            <Text style={{color: 'rgb(255,255,255)'}}>SeeaFood</Text> 
+        </View>
+    );
+};
+
+export default App;
